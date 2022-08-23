20ff2ad8c021c047a9b906e805287b77926987d0
JH <jinhyuk@unist.ac.kr>
2022-08-24 03:14:19 +0900

    fourier transform based score implemented

--- levy_stable_pytorch.py
+++ levy_stable_pytorch.py
@@ -1,10 +1,10 @@
 
 import torch
-import numpy as np
 from Cython import inline
 from torchquad import set_up_backend  # Necessary to enable GPU support
 from torchquad import Simpson # The available integrators
 from torch.distributions.exponential import Exponential
+from torch_dictionary import TorchDictionary
 
 if torch.cuda.is_available():
     set_up_backend("torch", data_type="float32")
@@ -100,7 +100,7 @@ class LevyStable:
         simp = Simpson()
         intg = simp.integrate(f, dim=1, N=999, integration_domain=[[1e-7, torch.pi / 2 - 1e-7]])
 
-        ret = alpha * intg / np.pi / torch.abs(torch.tensor(alpha - 1)) / x
+        ret = alpha * intg / torch.pi / torch.abs(torch.tensor(alpha - 1)) / x
 
         if torch.any(x == 0):
             gamma_func = lambda a: torch.exp(torch.special.gammaln(a))
@@ -121,8 +121,57 @@ class LevyStable:
         else:
             raise NotImplementedError("not yet implemented when alpha <= 1 or beta != 0 ")
 
-    def _score_simple(self, x: torch.Tensor, alpha):
+    def score_fourier_transform(self, x: torch.Tensor, alpha, beta=0, t0=10, Fs=30, sigma_1=1, sigma_2=1):
 
+        def cft(g, f):
+            """Numerically evaluate the Fourier Transform of g for the given frequencies"""
+
+            simp = Simpson()
+            intg = simp.integrate(lambda t: g(t) * torch.exp(-2j * torch.pi * f * t), dim=1, N=10001,
+                                  integration_domain=[[-5, 5]])
+            return intg
+
+        def g1(t):
+            return torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
+                -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+
+        def g2(t):
+            return (-2j * torch.pi * t) * torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
+                -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+
+        t = torch.arange(-t0, t0, 1. / Fs)
+        f = torch.linspace(-Fs / 2, Fs / 2, len(t))
+
+        G_exact1 = cft(g1, f)
+        G_exact2 = cft(g2, f)
+        
+        score = G_exact2 / G_exact1
+
+        score_dict = TorchDictionary(keys=f, values=score)
+        return score_dict.get(x, linear_approx=True)
+
+    def score_fft(self, x: torch.Tensor, alpha, beta=0, t0=10, Fs=30, sigma_1=1, sigma_2=1):
+
+        def g1(t):
+            return torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
+                -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+
+        def g2(t):
+            return (-2j * torch.pi * t) * torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
+                -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+
+        t = torch.arange(-t0, t0, 1. / Fs)
+        f = torch.linspace(-Fs / 2, Fs / 2, len(t))
+
+        approx1 = torch.fft.fftshift(torch.fft.fft(g1(t)) * torch.exp(2j * torch.pi * f * t0) * 1 / Fs)
+        approx2 = torch.fft.fftshift(torch.fft.fft(g2(t)) * torch.exp(2j * torch.pi * f * t0) * 1 / Fs)
+
+        score = approx2 / approx1
+
+        score_dict = TorchDictionary(keys=f, values=score)
+        return score_dict.get(x, linear_approx=True)
+
+    def _score_simple(self, x: torch.Tensor, alpha):
         x.requires_grad_()
         log_likelihood = torch.log(self._pdf_simple(x, alpha))
         grad = torch.autograd.grad(log_likelihood.sum(), x, allow_unused=True)[0]
