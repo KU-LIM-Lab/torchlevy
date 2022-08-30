8473300cd4812fd0bcb826501757768187b1bb8c
JH <jinhyuk@unist.ac.kr>
2022-08-30 20:39:28 +0900

    .

--- torchlevy/__init__.py
+++ torchlevy/__init__.py
@@ -5,4 +5,4 @@ from torchquad import set_up_backend  # Necessary to enable GPU support
 import torch
 
 if torch.cuda.is_available():
-    set_up_backend("torch", data_type="float32")
+    set_up_backend("torch")
