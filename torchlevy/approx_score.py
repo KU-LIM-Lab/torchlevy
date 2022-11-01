f407466f52ae7fe7bef3c909227982470aa86e6a
JH <jinhyuk@unist.ac.kr>
2022-11-01 14:20:12 +0900

    rollback ReLS

--- torchlevy/approx_score.py
+++ torchlevy/approx_score.py
@@ -8,28 +8,15 @@ from .score_hpo_result import rectified_hpo_result, real_linear_hpo_result, expo
 def get_approx_score(x, alpha, is_mid_real_score=True):
     if alpha == 2:
         return - x / 2
-    beta_hat = alpha - 1
 
-    levy = LevyStable()
-    func = lambda x: levy.score(x, alpha=alpha)
-    extreme_pts = get_extreme_pts(func)
+    extreme_pts, c, t = _get_c_t(alpha)
 
-    if len(extreme_pts) != 2:
-        idx = len(extreme_pts) // 2
-        extreme_pts = [-extreme_pts[idx], extreme_pts[idx]]
-
-
-    # print(1111, extreme_pts[1])
-    # print(2222, levy.pdf(torch.Tensor(extreme_pts), alpha)[1])
-
-    # approx_score
-    c_hat = - levy.score(torch.Tensor(extreme_pts), alpha)[1] / extreme_pts[1] ** beta_hat
-    approx_score = - torch.sign(x) * c_hat * torch.abs(x) ** beta_hat
+    approx_score = (-torch.sign(x) * c * (x ** t))[x >= 0]
 
     if is_mid_real_score and alpha < 2:
         levy = LevyStable()
         score = levy.score(x, alpha)
-        approx_score[torch.abs(x) <= extreme_pts[-1]] = score[[torch.abs(x) <= extreme_pts[-1]]]
+        approx_score[torch.abs(x) <= extreme_pts[-1] * 0.8] = score[[torch.abs(x) <= extreme_pts[-1] * 0.8]]
 
     return approx_score
 
@@ -155,11 +142,11 @@ def real_linear_tuning_score(x, alpha):
     return score
 
 
-
 def fitting_gen_gaussian_score(x, alpha):
     def gen_gaussian_score(x, beta, scale=1):
         return - beta * torch.sign(x) * torch.abs(x / scale) ** (beta - 1) / scale
-    return gen_gaussian_score(x, beta=alpha, scale=2 ** ((alpha+1) / 3))
+
+    return gen_gaussian_score(x, beta=alpha, scale=2 ** ((alpha + 1) / 3))
 
 
 def generalized_gaussian_score(x, alpha):
@@ -171,5 +158,3 @@ def generalized_gaussian_score(x, alpha):
     beta_hat = exponent_alpha_related_result[alpha]["beta_hat"]
     score = - torch.sign(x) * c_hat * torch.abs(x) ** beta_hat
     return score
-
-
