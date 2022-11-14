772fb0c8339acaca42b5c0444822426827a9b44f
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2022-11-14 14:14:08 +0900

    Update levy_gaussian.py

--- torchlevy/levy_gaussian.py
+++ torchlevy/levy_gaussian.py
@@ -5,6 +5,7 @@ from torchquad import Simpson # The available integrators
 from functools import lru_cache
 from .util import gaussian_score
 
+import matplotlib.pyplot as plt
 
 class LevyGaussian:
     def __init__(self, alpha, sigma_1, sigma_2, beta=0, t0=30, Fs=100, type="cft"):
@@ -16,10 +17,7 @@ class LevyGaussian:
 
         if type == "cft":
             self.score_dict = _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs)
-            self.score_dict_large_Fs = _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs=300)
-        elif type == "fft":
-            self.score_dict = _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs=300)
-            self.score_dict_large_Fs = self.score_dict
+            self.score_dict_large_Fs = _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs=300)
         else:
             raise RuntimeError(f"type :{type} isn't implemented")
 
@@ -32,7 +30,6 @@ class LevyGaussian:
         score = self.score_dict.get(x, linear_approx=True)
         score_for_large_x = self.score_dict_large_Fs.get(x, linear_approx=True)
 
-        score[torch.abs(x) > 18] = score_for_large_x[torch.abs(x) > 18]
         return score
 
 
@@ -40,48 +37,52 @@ class LevyGaussian:
 @lru_cache(maxsize=1050)
 def _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs):
     def cft(g, f):
+
         """Numerically evaluate the Fourier Transform of g for the given frequencies"""
 
         simp = Simpson()
-        intg = simp.integrate(lambda t: g(t) * torch.exp(-2j * torch.pi * f * t), dim=1, N=10001,
-                              integration_domain=[[-5, 5]])
+        intg = simp.integrate(lambda t: g(t) * torch.exp(-1j * f * t), dim=1, N=10001,
+                              integration_domain=[[-100, 100]])
         return intg
 
     def g1(t):
-        return torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
-            -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+        return torch.exp( -torch.pow(torch.abs(t), alpha))
 
     def g2(t):
-        return (-2j * torch.pi * t) * torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
-            -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
+        return -(1j  * t) *torch.abs(t+1e-20)**(alpha-2) *torch.exp(-1/2*torch.pow(torch.abs(t*(sigma_1/sigma_2) ), 2))*torch.exp(-torch.pow(torch.abs(t ), alpha))
 
     t = torch.arange(-t0, t0, 1. / Fs)
     f = torch.linspace(-Fs / 2, Fs / 2, len(t))
 
-    G_exact1 = cft(g1, f)
-    G_exact2 = cft(g2, f)
+    G_exact1 = cft(g1, f).real
+    G_exact2 = cft(g2, f).real
 
     score = G_exact2 / G_exact1
 
     return TorchDictionary(keys=f, values=score)
 
-@lru_cache(maxsize=1050)
-def _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs):
-
-    def g1(t):
-        return torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
-            -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
-
-    def g2(t):
-        return (-2j * torch.pi * t) * torch.exp(-1 / 2 * (2 * torch.pi * t * sigma_1) ** 2) * torch.exp(
-            -torch.pow(2 * torch.pi * torch.abs(t * sigma_2), alpha))
-
-    t = torch.arange(-t0, t0, 1. / Fs)
-    f = torch.linspace(-Fs / 2, Fs / 2, len(t))
-
-    approx1 = torch.fft.fftshift(torch.fft.fft(g1(t)) * torch.exp(2j * torch.pi * f * t0) * 1 / Fs)
-    approx2 = torch.fft.fftshift(torch.fft.fft(g2(t)) * torch.exp(2j * torch.pi * f * t0) * 1 / Fs)
-
-    score = approx2 / approx1
-
-    return TorchDictionary(keys=f, values=score)
+# def cft(g, f):
+#
+#         """Numerically evaluate the Fourier Transform of g for the given frequencies"""
+#
+#         simp = Simpson()
+#         intg = simp.integrate(lambda t: g(t) * torch.exp(-1j * f * t), dim=1, N=10001,
+#                               integration_domain=[[-100, 100]])
+#         return intg
+#
+# def g1(t):
+#         return torch.exp( -torch.pow(torch.abs(t), alpha))
+#
+# def g2(t):
+#         return -(1j  * t) *torch.abs(t+1e-20)**(alpha-2) *torch.exp(-torch.pow(torch.abs(t ), alpha))
+#
+# alpha=1.5
+# t = torch.arange(-100, 100, 1/100)
+# f = torch.linspace(-100/ 2, 100 / 2, len(t))
+#
+# G_exact1 = cft(g1, f).real
+# G_exact2 = cft(g2, f).real
+#
+# score = G_exact2 / G_exact1
+# plt.plot(f, score)
+# plt.show()
