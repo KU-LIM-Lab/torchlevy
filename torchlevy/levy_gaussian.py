16b2e2ec6a2bf510bb0db675197c531227b3a801
JH <jinhyuk@unist.ac.kr>
2022-11-16 13:16:02 +0900

    edit levy_gaussian.py by eunbi

--- torchlevy/levy_gaussian.py
+++ torchlevy/levy_gaussian.py
@@ -11,19 +11,17 @@ from typing import Union
 
 
 class LevyGaussian:
-    def __init__(self, alpha, sigma_1, sigma_2, beta=0, t0=30, Fs=100, type="fft"):
+    def __init__(self, alpha, sigma_1, sigma_2, beta=0, t0=30, Fs=100, type="cft"):
         self.alpha = alpha
         self.beta = beta
 
         if beta != 0:
             raise NotImplementedError(f"beta != 0 not yet implemented")
 
-        if type == "cft":
-            warnings.warn("cft will be deprecated. please use fft")
 
         if type in ["cft", "fft"]:
-            self.score_dict = _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs)
-            self.score_dict_large_Fs = _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs=300)
+            self.score_dict = _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs)
+            self.score_dict_large_Fs = _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs=300)
         else:
             raise RuntimeError(f"type :{type} isn't implemented")
 
@@ -35,23 +33,22 @@ class LevyGaussian:
         score = self.score_dict.get(x, linear_approx=True)
         score_for_large_x = self.score_dict_large_Fs.get(x, linear_approx=True)
 
-        score[torch.abs(x) > 18] = score_for_large_x[torch.abs(x) > 18]
         return score
 
 
 def levy_gaussian_score(alpha: float, x: torch.Tensor, sigma1s: Union[list, torch.Tensor],
-                        sigma2s: Union[list, torch.Tensor], beta=0, t0=30, Fs=100):
+                        sigma2s: Union[list, torch.Tensor], beta=0, t0=30, Fs=100, mode='fraction'):
     score = torch.zeros_like(x)
 
     for i, (s1, s2) in enumerate(zip(sigma1s, sigma2s)):
-        score_dict = _get_score_dict_fft(alpha, s1, s2, beta, t0, Fs)
+        score_dict = _get_score_dict_cft(alpha, s1, s2, beta, t0, Fs, mode=mode)
         score[i] = score_dict.get(x[i], linear_approx=True)
 
     return score
 
 
 @lru_cache(maxsize=1050)
-def _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs):
+def _get_score_dict_cft(alpha, sigma_1, sigma_2, beta, t0, Fs, mode='fraction'):
     def cft(g, f):
         """Numerically evaluate the Fourier Transform of g for the given frequencies"""
 
@@ -61,11 +58,15 @@ def _get_score_dict_fft(alpha, sigma_1, sigma_2, beta, t0, Fs):
         return intg
 
     def g1(t):
-        return torch.exp(-torch.pow(torch.abs(t), alpha))
+        return torch.exp(-torch.pow(torch.abs(t), alpha))* torch.exp(-1 / 2 * torch.pow(torch.abs(t * (sigma_1 / (sigma_2))), 2))
 
     def g2(t):
-        return -(1j * t) * torch.abs(t + 1e-20) ** (alpha - 2) * torch.exp(
-            -1 / 2 * torch.pow(torch.abs(t * (sigma_1 / sigma_2)), 2)) * torch.exp(-torch.pow(torch.abs(t), alpha))
+        if mode=='score':
+
+            a = -(1j * t) *  torch.exp(-1 / 2 * torch.pow(torch.abs(t * (sigma_1 / (sigma_2))), 2)) * torch.exp(-torch.pow(torch.abs(t),alpha))
+        else:
+            a= -(1j * t) * torch.abs(t + 1e-20) ** (alpha - 2) * torch.exp(-1 / 2 * torch.pow(torch.abs(t * (sigma_1 / (sigma_2))), 2)) * torch.exp(-torch.pow(torch.abs(t), alpha))
+        return a
 
     t = torch.arange(-t0, t0, 1. / Fs)
     f = torch.linspace(-Fs / 2, Fs / 2, len(t))
